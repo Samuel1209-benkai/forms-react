62d73d4bb5b5b3b14c55a77931e384750cba348e
Samuel1209-benkai <samuelsenouvo@gmail.com>
2022-11-30 17:07:03 +0100

    test for Job

--- src/App.js
+++ src/App.js
@@ -1,50 +1,13 @@
-import React, { useState  , useEffect } from 'react';
-// import { Route , Routes } from 'react-router-dom'
-// import Forms from './forms';
-// import Navbar from './navBar'
-// import Home from './page/home';
-// import Pricing from './page/pricing';
-// import About from './page/About';
-// import SignUp from './connexion';
+import React from 'react';
+import Forms from './forms';
+
 
 
 function App() { 
-  // const isconnect = true 
-  const [starWarsData , setStarWarsData] = useState ({})
-  const [count, setCount] = React.useState(1)
   
-    
-  // 1. GET the data (fetch or axios)
-    // 2. Save the data to state 
-
-   
-     useEffect ( () => {
-      console.log("Effect ran ")
-      fetch(`https://swapi.dev/api/people/${count}`)
-      .then (res=> res.json())
-      .then ( data => setStarWarsData(data)); 
-     },[count])
   return (
     <div className="App h-full">
-  {/* {
-    isconnect? 
-    
-<SignUp/> :
-<div>    
-    <Navbar/>
-    <div>
-    <Routes>
-      <Route path="/" element ={<Home/>}/>
-      <Route path="/pricing" element ={<Pricing/>}/>
-      <Route path="/about" element ={<About/>}/>
-      <Route path="/form" element ={<Forms/>}/>
-    </Routes>
-   </div>
-   </div>
-  }  */}
-  <pre>{JSON.stringify(starWarsData, null, 2)}</pre>
-  <h2>The count is {count}</h2>
-            <button className='bg-blue-500 m-5' onClick={() => setCount(prevCount => prevCount + 1)}>Add</button>
+      <Forms/>
     </div>
     );
   }
--- src/forms.js
+++ src/forms.js
@@ -1,74 +1,94 @@
-import React, {useState } from "react";
-
-function Forms (){
-
-     const  [ formsData , setFormData]= useState({
-        firstName :"",
-        lastName : "",
-        email : "" ,
-        favColor : "",
-     });
-
-     console.log(formsData.favColor);
-    function  changed(event){
-        setFormData (prevState =>{
+import React, { useRef, useState, useEffect } from "react";
+function Forms() {
+    const userRef = useRef();
+    const [formsData, setFormData] = useState({
+        firstName: "",
+        lastName: "",
+        telephone: "",
+    });
+    const [nameArray, setName] = useState([])
+    useEffect(() => {
+        userRef.current.focus();
+    }, []);
+    function changed(event) {
+        setFormData(prevState => {
             return {
                 ...prevState,
-                [event.target.name] : event.target.value ,
+                [event.target.name]: event.target.value,
             }
         })
-     }
-     function submeted(event){
-        event.preventDefaulte();
-        // submitToApi( formsData)
-        console.log(formsData)
-     }
-    return(<div>
-        <form onSubmit={submeted}>
-            <input  className=" border border-black m-4 p-3 rounded "
-            type="text"
-            placeholder="FirstName"
-            onChange={changed}
-            name="firstName" 
-            value={formsData.firstName}
+    }
+    async function handleSubmit(e) {
+        e.preventDefault();
+        console.log(formsData);
+        setName(oldArray => [...oldArray, formsData])
+        nameArray.sort()
+        console.log(nameArray)
+    }
+    function onHeaderClick(e) {
+        setName([...nameArray].sort((a, b) => a.firstName.localeCompare(b.firstName)));
+        console.log(nameArray)
+    }
+    return (<div className="m-10">
+        <form onSubmit={handleSubmit}>
+            <label>Nom</label>
+            <input className=" border border-black m-4 p-3 rounded "
+                type="text"
+                placeholder="FirstName"
+                ref={userRef}
+                onChange={changed}
+                name="firstName"
+                value={formsData.firstName}
             />
-
-<input  className=" border border-black m-4 p-3 rounded "
-            type="text"
-            placeholder="LastName"
-            onChange={changed}
-            name="lastName" 
-            value={formsData.lastName}
-            />
-
-<input  className=" border border-black m-4 p-3 rounded "
-            type="email"
-            placeholder="email"
-            onChange={changed}
-            name="email" 
-            value={formsData.email}
-            />
-<br/>
-
-<label className="m-4" htmlFor="favColor">What is your favorite color?</label>
             <br />
-            <select className="m-4"
-            id="favColor"
-            value={FormData.favColor}
-            onChange={changed}
-            name="favColor"
-            >
-                <option value="red">Red</option>
-                <option value="orange">Orange</option>
-                <option value="yellow">Yellow</option>
-                <option selected value="green">Green</option>
-                <option value="blue">Blue</option>
-                <option value="indigo">Indigo</option>
-                <option value="violet">Violet</option>
-            </select>
-            <br/>
-            <button type="Submit" className="border m-4 p-1  px-3  bg-blue-600  text-white rounded-md " > Submit</button>
+            <label>prenom </label>
+            <input className=" border border-black m-4 p-3 rounded "
+                type="text"
+                placeholder="LastName"
+                onChange={changed}
+                name="lastName"
+                value={formsData.lastName}
+            /><br />
+            <label>Telephone </label>
+            <input className=" border border-black m-4 p-3 rounded "
+                type="text"
+                placeholder="email"
+                onChange={changed}
+                name="telephone"
+                value={formsData.telephone}
+            />
+            <br />
+            <button type="Submit" className="border ml-80 m-4 p-1  px-3  bg-blue-600  text-white rounded-md " > Submit</button>
         </form>
+        <br />
+        <table className="border-collapse border border-slate-400  ... ">
+            <thead>
+                <tr >
+                    <th className="border border-slate-400 px-10 p-2">
+                        <h3>Prenom</h3>
+                    </th>
+                    <th className="border border-slate-400  px-10 p-2">
+                        <h3>Nom</h3>
+                    </th>
+                    <th className="border border-slate-400  px-10 p-2">
+                        <h3>Telephone</h3>
+                    </th>
+                </tr>
+            </thead>
+            <tbody>
+                {nameArray.map(item => {
+                    return (
+                        <tr>
+                            <td className="border border-slate-400  px-10 p-2"> {item.firstName}</td>
+                            <td className="border border-slate-400  px-10 p-2">{item.lastName}</td>
+                            <td className="border border-slate-400  px-10 p-2">{item.telephone}</td>
+                        </tr>
+                    )
+                })}
+            </tbody>
+        </table>
+
+        <button onClick={onHeaderClick} className="border ml-80 m-4 p-1  px-3  bg-green-600  text-white rounded-md "  > Order</button>
     </div>);
 }
 
